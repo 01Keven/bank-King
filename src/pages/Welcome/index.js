f25d3f5693de583a9660bbeb571516900195d895
Keven Christian <113730071+01Keven@users.noreply.github.com>
2023-10-04 18:26:49 -0300

    :construction:  preparando SVGs

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,11 +1,21 @@
 // componente funcional - rnfunction
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+    View, 
+    Text,
+    StyleSheet
+} from 'react-native'
 
 export default function Welcome() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Tela Bem-vindo</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-
+// adicionar o resto das paginas
 import Welcome from '../pages/Welcome'
-import SignIn from '../src/pages/SignIn'
+import SignIn from '../pages/SignIn'
 
 const Stack = createNativeStackNavigator();
 
@@ -12,11 +12,13 @@ export default function Routes(){
             <Stack.Screen
                 name="Welcome"
                 component={Welcome}
+                options={{ headerShown: false }} // escondendo header
             />
 
             <Stack.Screen
                 name="SignIn"
                 component={SignIn}
+                options={{ headerShown: false }}
             />
         </Stack.Navigator>
     )
