66dc906ffa9ce314ba73a18ec54c474bbfd3ef7b
Keven Christian <113730071+01Keven@users.noreply.github.com>
2023-10-04 17:44:43 -0300

    :construction:  criando componentes
    welcome, login

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,11 @@
+// componente funcional - rnfunction
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Welcome() {
+  return (
+    <View>
+      <Text>Tela Bem-vindo</Text>
+    </View>
+  )
+}
