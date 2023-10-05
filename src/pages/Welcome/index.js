2dab33197abc905e423d876980c93c9e94f6f9f0
Keven Christian <113730071+01Keven@users.noreply.github.com>
2023-10-05 18:53:22 -0300

    :construction: ajustando tela inicial

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -5,27 +5,76 @@ import {
     Text,
     StyleSheet,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    ImageBackground
 } from 'react-native'
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      
-      <View>
-        <Image // Logo pequena/branca
+      <ImageBackground source={require('../../assets/BackgroundInicio.png')} style={styles.imageBackground}>
+      <Image source={require('../../assets/LogoPequena.png')} style={styles.imageForeground}/>
+      <Text style={styles.title}>Bem Vindo</Text>
+    </ImageBackground>
+
+
+
+      {/* <ImageBackground  
+          source={require('../../assets/BackgroundInicio.png')}
+          style={styles.containerImageBg}  
+      />
+      {<Image // Logo pequena/branca
           source={require('../../assets/LogoPequena.png')}
-          style={{ width: '100%' }}
+          style={styles.containerLogoPequena}
+          
           resizeMode='contain'
-        />  
+        />}
+
+      <Text style={styles.title}>Bem Vindo</Text> */}
+        
+
+      <View style={styles.containerForm}>
+        <Text style={styles.text}>Comece a jornada rumo ao seu sucesso financeiro com o BankKing.</Text>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Iniciar</Text>
+        </TouchableOpacity>
+
       </View>
+
+      
     
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
     container:{
-
+      flex: 1,
+      flexDirection: 'column'
+      //backgroundColor: "#555555"
+    },
+    imageBackground:{
+      flex: 2,
+      width: 428,
+      height: 651,
+      resizeMode: 'cover',
+      justifyContent: 'center',
+      alignItems: 'center'
+      
+    },
+    imageForeground:{
+      width: 94,
+      height: 55.49
+    },
+    containerForm:{
+      
+      flex: 1,
+      backgroundColor: '#fff'
+    },
+    title:{
+      color: 'white',
+      textAlign: 'center'
     }
+
 })
